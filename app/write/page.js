7bd7a01d6fefd11e693b0bc611ee693355e1a022
sabriyeuregen <sabriyeuregen@gmail.com>
2024-01-16 10:51:12 +0300

    write pagein içindekiler kaldırıldı

--- app/write/page.js
+++ app/write/page.js
@@ -1,32 +1,33 @@
 "use client";
 import { useState } from "react";
 import styles from "./write.module.css";
-import Image from "next/image";
-import ReactQuill from "react-quill";
-import "react-quill/dist/quill.bubble.css";
-import { useSession } from "next-auth/react";
-import { useRouter } from "next/router";
+// import Image from "next/image";
+// import ReactQuill from "react-quill";
+// import "react-quill/dist/quill.bubble.css";
+// import { useSession } from "next-auth/react";
+// import { useRouter } from "next/router";
 
 const WritePage = () => {
 
-  const [open, setOpen] = useState(false);
-  const [value, setValue] = useState("");
+  // const [open, setOpen] = useState(false);
+  // const [value, setValue] = useState("");
 
-  const { data, status } = useSession;
+  // const { data, status } = useSession;
 
-  const router = useRouter();
+  // const router = useRouter();
 
-  if(status === "loading") {
-   return <div className={styles.loading}>Loading...</div>
-  }
+  // if(status === "loading") {
+  //  return <div className={styles.loading}>Loading...</div>
+  // }
 
-  if(status === "authenticated") {
-    router.push("/")
-  }
+  // if(status === "authenticated") {
+  //   router.push("/")
+  // }
 
   return (
     <div className={styles.container}>
-      <input type="text" placeholder="Title" />
+      <h1>Write</h1>
+      {/* <input type="text" placeholder="Title" />
       <div className={styles.editor}>
         <button className={styles.button} onClick={()=>setOpen(!open)}>
           <Image src={""} alt="" width={16} height={16} />
@@ -50,7 +51,7 @@ const WritePage = () => {
           onChange={setValue}
           placeholder="Tell your story."
         />
-      </div>
+      </div> */}
     </div>
   );
 };
