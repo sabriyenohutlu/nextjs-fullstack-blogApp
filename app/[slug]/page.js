dd7f7e7c8d87aeddbef06027bbaca4ba18311e51
sabriyeuregen <sabriyeuregen@gmail.com>
2024-01-16 11:23:23 +0300

    added photos and target blank

--- app/[slug]/page.js
+++ app/[slug]/page.js
@@ -1,42 +1,12 @@
 import Menu from "@/components/menu/Menu";
-import styles from "./singlePage.module.css";
 import Image from "next/image";
 import Comments from "@/components/comments/Comments";
+import NewPost from "@/components/newPost/NewPost";
 
-const SinglePage = ({ params }) => {
-  const { slug } = params;
+const SinglePage = () => {
 
   return (
-    <div className={styles.container}>
-      <div className={styles.infoContainer}>
-        <div className={styles.textContainer}>
-          <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
-          <div className={styles.user}>
-            <div className={styles.userImageContainer}>
-              <Image src="" alt="" fill className={styles.avatar} />
-            </div>
-            <div className={styles.userTextContainer}>
-              <span className={styles.username}>Jhon Doe</span>
-              <span className={styles.date}>01.01.2024</span>
-            </div>
-          </div>
-        </div>
-        <div className={styles.imageContainer}>
-          <Image src="" alt="" fill className={styles.image} />
-        </div>
-      </div>
-      <div className={styles.content}>
-        <div className={styles.post}>
-          <div
-            className={styles.description}
-          >
-            <div className={styles.comment}>
-              <Comments />
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <NewPost/>
   );
 };
 
